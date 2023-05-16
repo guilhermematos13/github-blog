ccad03e269408c60c89eb2188a97007b592a3476
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-16 12:45:46 -0300

    feat: tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
