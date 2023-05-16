5bbc28b7fdc28578f8aa9ddc92a0d286692610cb
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-16 12:48:33 -0300

    feat: config font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   purge: ['./index.html', './src/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
